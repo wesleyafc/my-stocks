6f9f0c1c0f800755accaf95d1f579fe2d63be278
wesleyafc <wesleyafc2010@gmail.com>
2022-04-05 12:01:56 -0300

    add new transaction with modal done

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import Modal from 'react-modal';
 import { AiOutlineClose } from "react-icons/ai";
 import { ModalContainer, InputContainer } from './styles'
+import axios from 'axios';
+import { api_url } from '../../apiURL'
 Modal.setAppElement('#root');
 const customStyles = {
 
@@ -30,16 +32,31 @@ const customStyles = {
 
 export function NewTransactionModal() {
 
+    const [actionsName, setActionName] = useState('')
+    const [quotasAmmount, setQuotasAmmount] = useState()
+    const [singleQuotaValue, setSingleQuotaValue] = useState()
+
 
     const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
 
-    function handleOpenNewTransactionModal(event) {
-        event.preventDefault();
+    function handleOpenNewTransactionModal() {
         setIsNewTransactionModalOpen(true);
     }
 
     function handleCloseNewTransactionModal() {
         setIsNewTransactionModalOpen(false);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newTransaction = {
+            actionsName,
+            quotasAmmount,
+            singleQuotaValue
+        };
+
+        await axios.post(api_url + "/transaction", newTransaction)
+        window.location.replace('/')
     }
 
     return (
@@ -64,20 +81,28 @@ export function NewTransactionModal() {
 
                 <InputContainer>
                     <h2>nova entrada</h2>
-                    <form >
+                    <form
+                        onSubmit={handleSubmit}
+                    >
                         <input
+                            type="text"
                             styles={"width: 100%"}
                             placeholder='nome da ação'
+                            onChange={e => setActionName(e.target.value)}
                         />
 
                         <input
                             type="number"
                             placeholder='quantidade de cotas adquiridas'
+                            onChange={e => setQuotasAmmount(e.target.value)}
+
                         />
 
                         <input
-                            type="number"
+                            type="number" step="0.01"
                             placeholder='valor unitario da cota'
+                            onChange={e => setSingleQuotaValue(e.target.value)}
+
                         />
 
                         <button type="submit">salvar</button>
