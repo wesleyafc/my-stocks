41a8b4be44916bb41a3ae1510f1c14063e73230e
wesleyafc <wesleyafc2010@gmail.com>
2022-04-30 10:59:17 -0300

    create context

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { ContextProvider } from './context/Context';
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <ContextProvider>
+        <App />
+      </ContextProvider>
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById('root')
